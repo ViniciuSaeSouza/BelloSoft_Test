dd8305d3aa78bfe4c3a9bf5b014b4fbbeef074e6
ViniciuSaeSouza <viniciusaesouza@gmail.com>
2025-08-26 17:22:44 -0300

    feat: migrations

---
+++ CoinGecko/Infrastructure/Migrations/20250826201235_InitialCreate-v1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreatev1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CryptoPrices",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Symbol = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PriceUsd = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    RetrievedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CryptoPrices", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CryptoPrices");
+        }
+    }
+}
