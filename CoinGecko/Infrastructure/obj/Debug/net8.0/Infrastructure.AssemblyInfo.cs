ff77c7132612f627496c50abd436e65773e91354
ViniciuSaeSouza <viniciusaesouza@gmail.com>
2025-08-26 17:36:29 -0300

    refactor: readme

--- CoinGecko/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ CoinGecko/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d66c0d76adf8f4a048c7391d0549a36bcf42c97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1602e830967e6dedaa817c87169b41abf305e3f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CoinGecko/Repositories/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ CoinGecko/Repositories/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d66c0d76adf8f4a048c7391d0549a36bcf42c97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1602e830967e6dedaa817c87169b41abf305e3f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
