a2e14680cd97605c9fff36db39ac651862496ad6
ViniciuSaeSouza <viniciusaesouza@gmail.com>
2025-08-27 16:33:41 -0300

    feat: first controller (crypto) and first endpoint for prices

---
+++ CoinGecko/API/Controllers/CryptoController.cs
@@ -0,0 +1,62 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CryptoController : ControllerBase
+    {
+        public readonly ICoinGeckoService _coinGeckoService;
+
+        public CryptoController(ICoinGeckoService coinGeckoService)
+        {
+            _coinGeckoService = coinGeckoService;
+        }
+
+        // GET: api/<PricesController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/<PricesController>/5
+        [HttpGet("/prices")]
+        public async Task<IActionResult> Get([FromQuery] string cryptoId, [FromQuery] string currency)
+        {
+            try
+            { 
+                Crypto crypto = await _coinGeckoService.GetCryptoInfoAsync(cryptoId, currency);
+                if (crypto != null) return Ok(crypto);
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        //// POST api/<PricesController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<PricesController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<PricesController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
