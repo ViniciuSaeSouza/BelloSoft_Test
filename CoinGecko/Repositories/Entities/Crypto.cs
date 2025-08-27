1e4a31698a64a46e7143fa2c543a237f93e51b11
ViniciuSaeSouza <viniciusaesouza@gmail.com>
2025-08-27 16:32:25 -0300

    refactor: Changed name and attributes of CoinPrice

---
+++ CoinGecko/Repositories/Entities/Crypto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Entities;
+
+public class Crypto
+{
+    public required string Id { get; set; }
+    public required string Currency { get; set; }
+    public decimal Price { get; set; }
+    public decimal Change24hr { get; set; }
+    public DateTime RetrievedAt { get; set; }
+}
